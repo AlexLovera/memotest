d772958c882a24adc6459d532476390ccc5d8d51
AlexLovera <alexander.lovera8@hotmail.com>
2021-01-09 20:06:27 -0300

    Agrega funciones para iniciar y parar el cronometro.

---
+++ src/main.js
@@ -0,0 +1,23 @@
+const $tiempoJugado=document.querySelector('#tiempo');
+//maximo 1 hora de juego, es decir si son 60 minutos, se termina el juego
+const $botonDeInicio = document.querySelector('#botonDeInicio');
+let intervaloTiempo;
+
+//para parar setInterval hay que usar el valor retornado por esta, usandolo como mparametro de clearInterval()
+function iniciarCronometro(){
+    let segundos=0;
+    let minutos=0;
+
+    intervaloTiempo = setInterval(function (){
+        segundos++;
+        if(segundos === 60){
+            minutos++;
+            segundos=0;
+        }
+        $tiempoJugado.textContent = `${minutos}:${segundos}`;
+    },1000);
+}
+
+function pararCronometro(setInterval){
+    clearInterval(setInterval);
+}
